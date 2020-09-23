af05ebe34fd974a683def48294e67a35cf300315
jfecoren <jfecoren@gmail.com>
2020-09-23 15:00:35 -0600

    Recirculation Demux Modified

--- src/recirculation_demux.v
+++ src/recirculation_demux.v
@@ -1,20 +1,20 @@
 module recirculation_demux(
-	input		din,				//Entrada del DEMUX
-	input 		active,				//Entrada selectora (salida de la AND)
-	output reg	out_demux,			//Salida al DEMUX_Striping
-	output reg	out_block			//Salida de vuelta
+	input [31:0]		din,				//Entrada del DEMUX
+	input 				active,				//Entrada selectora (salida de la AND)
+	output reg [31:0]	out_demux,			//Salida al DEMUX_Striping
+	output reg [31:0]	out_block			//Salida de vuelta
 );
 	
 	always @(*)
 		begin: ReDEMUX
 			case((active))
 				0 : begin
-					out_demux = 0;
+					out_demux = 'b0;
 					out_block = din;
 					end
 				1 : begin				
 					out_demux = din;
-					out_block = 0;
+					out_block = 'b0;
 					end
 			endcase
 		end
